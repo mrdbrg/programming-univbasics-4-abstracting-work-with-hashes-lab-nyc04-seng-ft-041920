be1a923518ffc429432a44d2815822a39eecfe9d
Marlon Braga <rdg.brg@gmail.com>
2020-04-29 19:24:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,11 +3,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  if hash[key]
-    return hash[key]
-  else  
-    return nil
-  end
+  hash[key]
 end
 
 def update_counting_hash(hash, key)
